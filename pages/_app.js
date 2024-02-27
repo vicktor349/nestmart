8bb2c1a991efbbc80a3731883358e0b83e1215f3
vicktoh <ayomidevictor349@gmail.com>
2024-02-27 16:36:09 +0100

    Clean up and added navbar, footer and layout: making footer and navbar universal

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps })
+{
+  return (
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home()
 {
   return (
-    <main>
-      hello world
-    </main>
+    <div>
+      <Head>
+        <title>Nest | Home</title>
+      </Head>
+    </div>
   );
 }
