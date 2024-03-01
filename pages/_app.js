092278a3aa0988abe827a379e264edce5eb66bb4
vicktoh <ayomidevictor349@gmail.com>
2024-03-01 13:12:50 +0100

    Changed the margin value

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps })
 {
   return (
-    <div className="mx-24">
+    <div className="mx-6">
       <Layout>
         <Component {...pageProps} />
       </Layout>
