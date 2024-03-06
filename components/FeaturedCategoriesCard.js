c51035725109be16b78ab68410ae626ee2e0e9d5
vicktoh <ayomidevictor349@gmail.com>
2024-03-06 10:57:30 +0100

    made height and width automatic

--- components/FeaturedCategoriesCard.js
+++ components/FeaturedCategoriesCard.js
@@ -6,7 +6,7 @@ const FeaturedCategoriesCard = ({ logo, item, amount, bg }) =>
     return (
         <div className={`${bg} w-48 h-64 flex justify-center items-center border border-transparent rounded-3xl`}>
             <div>
-                <Image src={logo} alt='Featured Image' width={100} height={100} className='text-center' />
+                <Image src={logo} alt='Featured Image' width={100} height={100} className='text-center h-auto w-auto' />
                 <section className='text-center'>
                     <h3 className='text-primaryText font-semibold text-xl'>{item}</h3>
                     <p className='text-[#7E7E7E]'>{amount} items</p>
