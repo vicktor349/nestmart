4c55a681f2771ed97a07356b23759cded3bf9bfb
vicktoh <ayomidevictor349@gmail.com>
2024-03-08 18:18:45 +0100

    Daily Deals component

---
+++ data/dealsData.js
@@ -0,0 +1,36 @@
+const dealsData = [
+    {
+        imageUrl: "/images/deals/quinoa.png",
+        text: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+        rating: "4.0",
+        vendor: "NestFood",
+        price: "$32.85",
+        changedPrice: "$33.8"
+    },
+    {
+        imageUrl: "/images/deals/lemon.png",
+        text: "Perdue Simply Smart Organics Gluten Free",
+        rating: "4.0",
+        vendor: "Old El Paso",
+        price: "$24.85",
+        changedPrice: "$26.8"
+    },
+    {
+        imageUrl: "/images/deals/greenpea.png",
+        text: "Signature Wood-Fired Mushroom and Caramelized",
+        rating: "3.0",
+        vendor: "Progresso",
+        price: "$12.85",
+        changedPrice: "$13.8"
+    },
+    {
+        imageUrl: "/images/deals/raspberry.png",
+        text: "Simply Lemonade with Raspberry Juice",
+        rating: "3.0",
+        vendor: "Yoplait",
+        price: "$15.85",
+        changedPrice: "$16.8"
+    },
+]
+
+export default dealsData;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import BestSales from "@/components/BestSales";
+import Deals from "@/components/Deals";
 import FeaturedCategories from "@/components/FeaturedCategories";
 import HeroBanner from "@/components/HeroBanner";
 import PopularProducts from "@/components/PopularProducts";
@@ -17,6 +18,7 @@ export default function Home()
         <FeaturedCategories />
         <PopularProducts />
         <BestSales />
+        <Deals />
       </div>
     </>
   );
