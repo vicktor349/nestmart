0c5d911620a5a4fb9c5822810fd10cff7f238acc
vicktoh <ayomidevictor349@gmail.com>
2024-03-11 11:21:56 +0100

    adjusted hero banner

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -5,7 +5,7 @@ const HeroBanner = () =>
 {
     return (
         <div className="bg-[url('/images/herobanner/heroBanner1.png')] mt-10 w-full ssm:h-[15rem] md:h-[27rem] lg:h-[33rem] border-transparent rounded-3xl flex items-center">
-            <div className="ml-24">
+            <div className="absolute left-[10%] md:leading-[4rem]">
                 <p className='text-primaryText ssm:text-2xl md:text-5xl font-semibold'>Fresh Vegetables</p>
                 <p className='text-primaryText ssm:text-2xl md:text-5xl font-semibold'>Big discount</p>
                 <p className='text-[#7E7E7E]'>Save up to 50% off on your first order</p>
