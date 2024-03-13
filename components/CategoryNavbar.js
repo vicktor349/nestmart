71db4bfecebe55257ed7aefe754209c9e2913b80
vicktoh <ayomidevictor349@gmail.com>
2024-03-13 15:41:01 +0100

    changed color of downward arrow and remove outmost div margin

--- components/CategoryNavbar.js
+++ components/CategoryNavbar.js
@@ -5,10 +5,10 @@ import React from 'react'
 const CategoryNavbar = () =>
 {
     return (
-        <div className='ssm:hidden lg:flex items-center justify-between my-6'>
+        <div className='ssm:hidden lg:flex items-center justify-between'>
             <div className="flex flex-row items-center bg-primary w-52 py-2 px-2 text-white border border-1 border-primary rounded-md hover:cursor-pointer">
                 <p className='font-bold'>Browse All Categories</p>
-                <Image src={"/images/icons/downwardarrow.svg"} alt='Downward arrow' height={100} width={100} className='h-5 w-5 mt-1' />
+                <Image src={"/images/icons/downwardarrow.svg"} alt='Downward arrow' height={100} width={100} className='h-3 w-3 mt-[0.4rem] ml-1' />
             </div>
             <div className='flex items-center'>
                 <Image src={"/images/icons/fire.svg"} width={100} height={100} className='w-5 h-5' alt='fire icon' />
