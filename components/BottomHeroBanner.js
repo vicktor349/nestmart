5e5273c0bdcf31008f4e09fd63be586f5258c490
vicktoh <ayomidevictor349@gmail.com>
2024-03-18 12:01:39 +0100

    bottom hero banner responsivity

--- components/BottomHeroBanner.js
+++ components/BottomHeroBanner.js
@@ -4,19 +4,18 @@ import React from 'react'
 const BottomHeroBanner = () =>
 {
     return (
-        <div className="bg-[url('/images/herobanner/heroBanner1.png')] mt-10 w-full ssm:h-[15rem] md:h-[27rem] lg:h-[33rem] border-transparent rounded-3xl flex items-center">
-            <div className='ml-24 leading-10'>
-                <p className='text-primaryText ssm:text-2xl md:text-5xl font-semibold'>Stay home & get your daily</p>
-                <p className='text-primaryText ssm:text-2xl md:text-5xl font-semibold'>needs from our shop</p>
-                <p className='text-[#7E7E7E]'>Start You'r Daily Shopping with <span className='text-primary hover:cursor-pointer hover:underline'>Nest Mart</span></p>
-                <div className='bg-white w-96 h-16 rounded-[6rem] flex items-center mt-4'>
-                    <Image src={"/images/icons/email.svg"} alt='email icon' width={100} height={100} className='w-6 h-6 mx-5' />
-                    <input type="text" className='rounded-[6rem] text-[#9ca3af] outline-none' placeholder='Your email address' />
-                    <button className='bg-primary h-16 rounded-[6rem] text-white w-72 l-6'>Subscribe</button>
+        <div className="bg-[url('/images/herobanner/heroBanner1.png')] mt-10 w-full ssm:h-[20rem] sm:h-[25rem] md:h-[27rem] lg:h-[33rem] border-transparent rounded-3xl flex flex-col justify-center">
+            <div className="ml-6 md:ml-24 md:text-left leading-10 h-72 w-fit">
+                <p className="text-primaryText ssm:text-xl sm:text-2xl lg:text-3xl xl:text-5xl font-semibold -ml-1">Stay home & get your daily</p>
+                <p className="text-primaryText ssm:text-xl sm:text-2xl lg:text-3xl xl:text-5xl font-semibold -ml-1">needs from our shop</p>
+                <p className="text-[#7E7E7E] mt-6 md:text-lg lg:text-xl">Start You'r Daily Shopping with <span className="text-primary hover:underline hover:cursor-pointer">Nest Mart</span></p>
+                <div className="bg-white mt-4 md:mt-6 ssm:w-[17rem] sm:w-full h-16 rounded-full flex items-center">
+                    <Image src={"/images/icons/email.svg"} alt="email icon" width="20" height="20" className="w-6 h-6 ssm:mx-2 sm:mx-5" priority />
+                    <input type="text" className="flex-grow rounded-full text-[#9ca3af] ssm:px-2 sm:px-4 outline-none" placeholder="Your email address" />
+                    <button className="bg-primary h-16 rounded-full text-white ssm:hidden sm:block sm:w-24 md:w-32 ml-2 md:ml-0">Subscribe</button>
                 </div>
             </div>
         </div>
-
     )
 }
 
