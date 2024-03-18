459cbdf2e0ea8213b1504fe1ac9156c8820546b8
vicktoh <ayomidevictor349@gmail.com>
2024-03-18 12:57:13 +0100

    made footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -6,14 +6,14 @@ import Link from 'next/link'
 const Footer = () =>
 {
     return (
-        <div className='grid grid-cols-footer text-primaryText leading-10 '>
+        <div className='grid ssm:grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-primaryText leading-10'>
             <div>
-                <div className="items-center ssm:hidden lg:flex">
+                <div className="items-center hidden lg:flex">
                     <Link href="/">
                         <Image src={"/images/logo.png"} width={100} height={100} alt="Nest Mart Logo" className='h-10 w-12' priority />
                     </Link>
                     <section className='leading-3 ml-2'>
-                        <Link className='text-primary text-4xl font-bold select-none ' href="/">Nest</Link>
+                        <Link className='text-primary text-4xl font-bold select-none' href="/">Nest</Link>
                         <p className="text-primaryText text-[10px]">MART & GROCERY</p>
                     </section>
                 </div>
@@ -40,7 +40,7 @@ const Footer = () =>
                     <p>10:00 - 18:00, Mon - Sat</p>
                 </section>
             </div>
-            <div className='ml-4'>
+            <div className='sm:col-span-1 lg:col-span-1'>
                 <h3 className='font-bold text-2xl'>Account</h3>
                 <p>Sign In</p>
                 <p>View Cart</p>
@@ -50,7 +50,7 @@ const Footer = () =>
                 <p>Shipping Details</p>
                 <p>Compare products</p>
             </div>
-            <div>
+            <div className='sm:col-span-1 lg:col-span-1'>
                 <h3 className='font-bold text-2xl'>Account</h3>
                 <p>Sign In</p>
                 <p>View Cart</p>
@@ -60,7 +60,7 @@ const Footer = () =>
                 <p>Shipping Details</p>
                 <p>Compare products</p>
             </div>
-            <div>
+            <div className='sm:col-span-1 lg:col-span-1'>
                 <h3 className='font-bold text-2xl'>Popular</h3>
                 <p>Milk & Flavoured Milk</p>
                 <p>Butter and Margarine</p>
@@ -70,33 +70,9 @@ const Footer = () =>
                 <p>Tea & Kombucha</p>
                 <p>Cheese</p>
             </div>
-            <div className='col-start-5 col-end-8 leading-[5rem]'>
-                <h3 className='font-bold text-2xl'>Install App</h3>
-                <p>From App Store or Google Play</p>
-                <div className='flex items-center'>
-                    <section className='bg-black rounded-md text-white flex items-center leading-5 w-52 justify-center'>
-                        <Image src={"/images/icons/applelogo.svg"} alt='Apple Logo' width={100} height={100} className='w-8 h-8 ml-' />
-                        <section className='py-2 ml-2'>
-                            <p>Downlad on</p>
-                            <h3 className='text-xl'>App Store</h3>
-                        </section>
-                    </section>
-                    <section className='bg-black rounded-md text-white flex items-center leading-5 w-52 justify-center ml-4'>
-                        <Image src={"/images/icons/playstore.svg"} alt='Google play stor Logo' width={100} height={100} className='w-8 h-8 ml-' />
-                        <section className='py-3 ml-2'>
-                            <p className="font-light">GET IT ON</p>
-                            <h3 className=''>Google Play</h3>
-                        </section>
-                    </section>
-                </div>
-                <p>Secured Payment Gateways</p>
-                <div className='flex items-center'>
-                    <Image src={"/images/icons/visa.svg"} alt='Visa Logo' width={100} height={100} className='w-16 h-16' />
-                    <Image src={"/images/icons/mastercard.svg"} alt='Mastercard Logo' width={100} height={100} className='w-16 h-16 mx-6' />
-                    <Image src={"/images/icons/maestro.svg"} alt='Maestro Logo' width={100} height={100} className='w-16 h-16' />
-                </div>
-            </div>
         </div>
+
+
     )
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) =>
             <Navbar />
             <CategoryNavbar />
             {children}
-            {/* <Footer />   */}
+            <Footer />
         </div>
     )
 }
