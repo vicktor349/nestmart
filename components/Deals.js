37f65f7b0321ed67600b319dbdd1d296e728a5e2
vicktoh <ayomidevictor349@gmail.com>
2024-03-26 12:15:56 +0100

    increased width and reduced padding

--- components/Deals.js
+++ components/Deals.js
@@ -9,28 +9,28 @@ const Deals = () =>
     <>
       <div className='mt-10'>
         <h2 className='text-3xl font-semibold text-primaryText'>Deals of The Day</h2>
-        <div className='grid grid-cols-100 gap-5 select-none'>
+        <div className='grid grid-cols-100 gap-5 select-none mt-3'>
           {
             dealsData.map((data, id) =>
             {
               return (
                 <div key={id}>
-                  <Image src={data.imageUrl} className="h-fit w-full mx-auto mt-5 object-contain" alt='Product Image' width={500} height={500} />
-                  <div className='mx-auto w-64 bg-white shadow-md relative -mt-28 rounded-2xl'>
-                    <div className='px-4 py-1'>
+                  <Image src={data.imageUrl} className="h-fit w-full mx-auto mt-5 object-contain bg-cover bg-center" alt='Product Image' width={500} height={500} />
+                  <div className='mx-auto w-64 bg-white shadow-md relative -mt-24 rounded-2xl'>
+                    <div className='px-2 py-1'>
                       <p className='text-sm text-[#ADADAD] w-full bg-white'>{data.category}</p>
-                      <h3 className='text-primaryText font-semibold hover:cursor-pointer'>{data.text}</h3>
+                      <h3 className='text-primaryText font-semibold hover:cursor-pointer text-md'>{data.text}</h3>
                       <section >
                         <section className='flex justify-between'>
-                          <Image src={"/images/popular/rating.png"} alt='Rating' width={500} height={500} className='object-contain h-auto w-auto' />
+                          <Image src={"/images/popular/rating.png"} alt='Rating' width={500} height={500} className='object-contain h-auto w-auto bg-cover bg-center' />
                           <p className='text-[#B6B6B6]'>({data.rating})</p>
                         </section>
                         <p>By <span className='text-[#3BB77E] hover:cursor-pointer hover:underline'>{data.vendor}</span></p>
                         <section className='flex items-center mb-2 text-md'>
-                          <p className='text-[#3BB77E] font-bold'>{data.price}</p>
-                          <p className='text-[#ADADAD] ml-2 font-bold'><s>{data.changedPrice}</s></p>
+                          <p className='text-[#3BB77E] font-semibold'>{data.price}</p>
+                          <p className='text-[#ADADAD] ml-2 font-semibold'><s>{data.changedPrice}</s></p>
                           <Link href="/" className='text-primary text-sm bg-[#DEF9EC] flex items-center py-2 px-5 ml-auto rounded-[0.2rem]'>
-                            <Image src={"/images/popular/cart.svg"} width={100} height={100} className='w-5 h-5' alt='Cart Logo' />
+                            <Image src={"/images/popular/cart.svg"} width={500} height={500} className='w-5 h-5' alt='Cart Logo' />
                             Add
                           </Link>
                         </section>
