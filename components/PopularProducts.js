382ea8a6fdd3a2f9a78a15884961d98c0a0e7848
vicktoh <ayomidevictor349@gmail.com>
2024-03-26 12:05:03 +0100

    font-bold to font-semibold

--- components/PopularProducts.js
+++ components/PopularProducts.js
@@ -27,8 +27,8 @@ const PopularProducts = () =>
                                         </section>
                                         <p>By <span className='text-[#3BB77E] hover:cursor-pointer hover:underline'>{data.vendor}</span></p>
                                         <section className='flex items-center mt-5 mb-8 text-md'>
-                                            <p className='text-[#3BB77E] font-bold'>{data.price}</p>
-                                            <p className='text-[#ADADAD] ml-2 font-bold'><s>{data.changedPrice}</s></p>
+                                            <p className='text-[#3BB77E] font-semibold'>{data.price}</p>
+                                            <p className='text-[#ADADAD] ml-2 font-semibold'><s>{data.changedPrice}</s></p>
                                             <Link href="/" className='text-primary text-sm bg-[#DEF9EC] flex items-center py-2 px-5 ml-auto rounded-[0.2rem]'>
                                                 <Image src={"/images/popular/cart.svg"} width={500} height={500} className='w-5 h-5' alt='Cart Logo' />
                                                 Add
