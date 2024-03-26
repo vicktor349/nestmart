deef2a380cd955fce5761413f3f05e78b95f0941
vicktoh <ayomidevictor349@gmail.com>
2024-03-26 12:00:20 +0100

    Centered Image and text of the category card

--- components/FeaturedCategoriesCard.js
+++ components/FeaturedCategoriesCard.js
@@ -5,10 +5,10 @@ const FeaturedCategoriesCard = ({ logo, item, amount, bg }) =>
 {
     return (
         <div className={`${bg} w-40 h-56 flex justify-center items-center border border-transparent rounded-3xl hover:shadow-md hover:cursor-pointer`}>
-            <div>
-                <Image src={logo} alt='Featured Image' width={100} height={100} className='text-center h-auto w-auto' />
+            <div className='grid place-items-center'>
+                <Image src={logo} alt='Featured Image' width={100} height={100} className='h-auto w-auto' />
                 <section className='text-center'>
-                    <h3 className='text-primaryText font-semibold text-xl'>{item}</h3>
+                    <h3 className='text-primaryText font-semibold text-lg'>{item}</h3>
                     <p className='text-[#7E7E7E]'>{amount} items</p>
                 </section>
             </div>
