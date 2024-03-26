3632175c0d8341d804a391c2d3de64537076419f
vicktoh <ayomidevictor349@gmail.com>
2024-03-26 12:28:03 +0100

    changed number of offers

--- components/Offers.js
+++ components/Offers.js
@@ -1,37 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
+import Offerdata from '@/data/offerdata'
 
 const Offers = () =>
 {
-    const Offerdata = [
-        {
-            imageUrl: "/images/offers/pricetag.png",
-            name: "Best prices & offers",
-            text: "24/7 amazing services"
-        },
-        {
-            imageUrl: "/images/offers/freedelivery.png",
-            name: "Free delivery",
-            text: "Orders $50 or more"
-        },
-        {
-            imageUrl: "/images/offers/deals.png",
-            name: "Great daily deal",
-            text: "When you sign up"
-        },
-        {
-            imageUrl: "/images/offers/assortment.png",
-            name: "Wide assortment",
-            text: "Mega Discounts"
-        },
-        {
-            imageUrl: "/images/offers/returns.png",
-            name: "Easy returns",
-            text: "Within 30 days"
-        },
-    ]
+
     return (
-        <div className='grid grid-cols-100 gap-5 my-16'>
+        <div className='grid grid-cols-100 gap-5 my-24'>
             {Offerdata.map((data, id) =>
             {
                 return (
---
+++ data/offerdata.js
@@ -0,0 +1,23 @@
+const Offerdata = [
+    {
+        imageUrl: "/images/offers/pricetag.png",
+        name: "Best prices & offers",
+        text: "24/7 amazing services"
+    },
+    {
+        imageUrl: "/images/offers/freedelivery.png",
+        name: "Free delivery",
+        text: "Orders $50 or more"
+    },
+    {
+        imageUrl: "/images/offers/deals.png",
+        name: "Great daily deal",
+        text: "When you sign up"
+    },
+    {
+        imageUrl: "/images/offers/returns.png",
+        name: "Easy returns",
+        text: "Within 30 days"
+    }
+]
+export default Offerdata
