413d236ff75fc7f9aaeaeda1ed935aaf14b6e9ab
vicktoh <ayomidevictor349@gmail.com>
2024-04-02 14:12:53 +0100

    increased size

--- pages/contact.js
+++ pages/contact.js
@@ -48,8 +48,8 @@ const contact = () =>
                 {/* HOW TO HELP DIV */}
                 <div className='lg:mx-16 mt-6 mb-16'>
                     <div className='leading-8'>
-                        <p className="text-primary font-bold ssm:text-xl lg:text-2xl xl:text-4xl text-center my-2">How can we help you?</p>
-                        <h3 className="text-primaryText sm:text-2xl lg:text-3xl xl:text-5xl font-semibold text-center my-4">Let us know how we can help you</h3>
+                        <p className="text-primary font-bold ssm:text-2xl lg:text-2xl xl:text-4xl text-center my-2">How can we help you?</p>
+                        <h3 className="text-primaryText text-3xl lg:text-3xl xl:text-5xl font-semibold text-center my-4">Let us know how we can help you</h3>
                         <p className="text-secondaryText w-full">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
                             elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus
