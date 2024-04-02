8060d192f8a101598c0c3f65df02bca1e1e1abcc
vicktoh <ayomidevictor349@gmail.com>
2024-04-02 13:46:21 +0100

    made mantine provider as tree component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps })
 {
   return (
     <div className="ssm:mx-4 sm:mx-8 md:mx-16 lg:mx-20 xl:mx-24 2xl:mx-48 font-Montserrat max-w-screen-2xl">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <MantineProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
     </div>
   );
 }
