89ca94fb0da0f86737e920f71832f3f0335985f0
vicktoh <ayomidevictor349@gmail.com>
2024-04-04 23:07:11 +0100

    finished shop page and modified the shop component connected to it

---
+++ data/dealsoftheday.js
@@ -0,0 +1,52 @@
+const dealsoftheday = [
+    {
+        imageUrl: "/images/deals/quinoa.png",
+        text: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+        rating: "4.0",
+        vendor: "NestFood",
+        price: "$32.85",
+        changedPrice: "$33.8",
+        days: "426",
+        hours: "01",
+        mins: "30",
+        sec: "12"
+    },
+    {
+        imageUrl: "/images/deals/lemon.png",
+        text: "Perdue Simply Smart Organics Gluten Free",
+        rating: "4.0",
+        vendor: "Old El Paso",
+        price: "$24.85",
+        changedPrice: "$26.8",
+        days: "822",
+        hours: "01",
+        mins: "30",
+        sec: "12"
+    },
+    {
+        imageUrl: "/images/deals/greenpea.png",
+        text: "Signature Wood-Fired Mushroom and Caramelized",
+        rating: "3.0",
+        vendor: "Progresso",
+        price: "$12.85",
+        changedPrice: "$13.8",
+        days: "1156",
+        hours: "01",
+        mins: "30",
+        sec: "12"
+    },
+    {
+        imageUrl: "/images/deals/raspberry.png",
+        text: "Simply Lemonade with Raspberry Juice",
+        rating: "3.0",
+        vendor: "Yoplait",
+        price: "$15.85",
+        changedPrice: "$16.8",
+        days: "398",
+        hours: "01",
+        mins: "30",
+        sec: "12"
+    },
+]
+
+export default dealsoftheday;
---
+++ pages/shop.js
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import React from 'react'
+import ShopProducts from '@/components/shop/ShopProducts'
+import { BiCategory } from "react-icons/bi";
+import { IoIosArrowDown } from "react-icons/io";
+import { GrSort } from "react-icons/gr";
+import DealsOfTheDay from '@/components/shop/DealsOfTheDay';
+import BottomHeroBanner from '@/components/BottomHeroBanner';
+
+const shop = () =>
+{
+    return (
+        <>
+            <Head>
+                <title>Nest | Shop</title>
+            </Head>
+            {/* SHOP BANNER */}
+            <div className='mb-28'>
+                <div className="bg-[url('/images/shopbanner/shopBanner.png')] bg-cover bg-center h-64 flex items-center mb-16 rounded-xl">
+                    <p className="text-primaryText text-5xl font-semibold ml-12">Shop</p>
+                </div>
+                {/* CATEGORY DIV */}
+                <div className='flex items-center py-5'>
+                    <h1 className='text-secondaryText'>We found <span className="text-primary">30</span> items for you!</h1>
+                    <div className='ml-auto flex items-center space-x-3'>
+                        <div className="border-secondaryText border border-solid text-secondaryText rounded-lg hover:cursor-pointer">
+                            <div className='p-2 flex items-center'>
+                                <BiCategory />
+                                <p>Show: 50</p>
+                                <IoIosArrowDown />
+                            </div>
+                        </div>
+                        <div className="border-secondaryText border border-solid text-secondaryText rounded-lg hover:cursor-pointer">
+                            <div className='p-2 flex items-center'>
+                                <GrSort />
+                                <p>Sort by: Featured</p>
+                                <IoIosArrowDown />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ShopProducts />
+                <DealsOfTheDay />
+                <BottomHeroBanner />
+            </div>
+
+        </>
+    )
+}
+
+export default shop
