e19c965788726b9d74b9074fb6ff2fe01d1ab410
vicktoh <ayomidevictor349@gmail.com>
2024-04-06 17:26:43 +0100

    gave each data a distinct id

--- data/bestsellingData.js
+++ data/bestsellingData.js
@@ -1,15 +1,17 @@
 const popularData = [
     {
+        id: 1,
         tag: "Save 35%",
         imageUrl: "/images/popular/rice.png",
         category: "Snack",
         text: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
-        rating: "4.0",
+        rating: "1.0",
         vendor: "NestFood",
         price: "$238.85",
         changedPrice: "$245.8",
     },
     {
+        id: 2,
         tag: "Sale",
         imageUrl: "/images/popular/meatball.png",
         category: "Hodo Foods",
@@ -20,33 +22,36 @@ const popularData = [
         changedPrice: "$55.8"
     },
     {
+        id: 3,
         tag: "Best sale",
         imageUrl: "/images/popular/corn.png",
         category: "Snack",
         text: "Angie's Boomchickapop Sweet & Salty Kettle Corn",
-        rating: "4.0",
+        rating: "2.5",
         vendor: "StarKist",
         price: "$48.85",
         changedPrice: "$52.8"
     }
     ,
     {
+        id: 4,
         tag: "New",
         imageUrl: "/images/popular/corn.png",
         category: "Snack",
         text: "Angie's Boomchickapop Sweet & Salty Kettle Corn",
-        rating: "4.0",
+        rating: "4.5",
         vendor: "StarKist",
         price: "$48.85",
         changedPrice: "$52.8"
     }
     ,
     {
+        id: 5,
         tag: "-10%",
         imageUrl: "/images/popular/corn.png",
         category: "Snack",
         text: "Angie's Boomchickapop Sweet & Salty Kettle Corn",
-        rating: "4.0",
+        rating: "5.0",
         vendor: "StarKist",
         price: "$48.85",
         changedPrice: "$52.8"
