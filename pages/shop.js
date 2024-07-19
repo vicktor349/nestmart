4c5cdbbc29d598307208a9edfdf0e529f6a91cff
vicktor349 <ayomidevictor349@gmail.com>
2024-07-19 14:02:49 +0100

    Fetching data from supabase

--- pages/shop.js
+++ pages/shop.js
@@ -1,14 +1,54 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ShopProducts from '@/components/shop/ShopProducts'
 import { BiCategory } from "react-icons/bi";
 import { IoIosArrowDown } from "react-icons/io";
 import { GrSort } from "react-icons/gr";
 import BottomHeroBanner from '@/components/BottomHeroBanner';
+import supabase from '@/config/supabase';
+
 
 
 const shop = () =>
 {
+    const [shopData, setShopData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() =>
+    {
+        getProducts();
+    }, []);
+
+    async function getProducts()
+    {
+        try
+        {
+            const { data, error } = await supabase.from('shopproductdata').select();
+            if (error)
+            {
+                console.error("Error fetching data:", error);
+            } else
+            {
+                setShopData(data)
+                setIsLoading(false)
+            }
+        } catch (err)
+        {
+            console.error("Unexpected error:", err);
+        }
+    }
+
+    if (isLoading || !shopData)
+    {
+        return (
+            <div className='fixed inset-0 flex items-center justify-center bg-white z-[999999]'>
+                <div className="loader">
+                </div>
+            </div>
+        )
+    }
+
 
     return (
         <>
@@ -22,7 +62,7 @@ const shop = () =>
                 </div>
                 {/* CATEGORY DIV */}
                 <div className='flex items-center py-5'>
-                    <h1 className='text-secondaryText ssm:hidden sm:flex'>We found <span className="text-primary">30</span> items for you!</h1>
+                    <h1 className='text-secondaryText ssm:hidden sm:flex'>We found <span className="text-primary mx-1"> {shopData.length} </span> items for you!</h1>
                     <div className='ml-auto flex items-center space-x-3'>
                         <div className="border-secondaryText border border-solid text-secondaryText rounded-lg hover:cursor-pointer">
                             <div className='p-2 flex items-center'>
@@ -40,7 +80,7 @@ const shop = () =>
                         </div>
                     </div>
                 </div>
-                <ShopProducts />
+                <ShopProducts shopData={shopData} />
                 <BottomHeroBanner />
             </div>
 
