850a9a517b6b738b213098c3dc231b259ad98b6f
vicktor349 <ayomidevictor349@gmail.com>
2024-07-19 14:09:50 +0100

    Removed margin from the page

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { MantineProvider } from "@mantine/core";
 export default function App({ Component, pageProps })
 {
   return (
-    <div className="ssm:mx-4 sm:mx-8 md:mx-16 lg:mx-20 xl:mx-24 2xl:mx-48 font-Montserrat max-w-screen-2xl">
+    <div>
       <MantineProvider>
         <Layout>
           <Component {...pageProps} />
