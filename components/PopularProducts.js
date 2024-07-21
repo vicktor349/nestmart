c9667982284ebd351b14959b384b4ec87de18f67
vicktor349 <ayomidevictor349@gmail.com>
2024-07-21 15:48:31 +0100

    Reduced the spacing contained for product on smaller screens

--- components/PopularProducts.js
+++ components/PopularProducts.js
@@ -53,30 +53,60 @@ const PopularProducts = () =>
     return (
         <div className='md:mt-6'>
             <h2 className='text-3xl font-semibold text-primaryText ssm:py-6 md:py-12'>Popular Products</h2>
-            <div className='grid ssm:grid-cols-50 sm:grid-cols-100 gap-5 select-none'>
+            <div className='grid sssms:grid-cols-2 sm:grid-cols-100 ssm:gap-2 sm:gap-5 select-none'>
                 {products.map((data, id) =>
                 {
                     return (
-                        <div onClick={() => route(data.id)} className='border border-1 border-[#ADADAD] rounded-xl hover:shadow-xl hover:cursor-pointer' key={id}>
-                            <p className="bg-[#F74B81] text-white w-20 py-[0.11rem] text-center rounded-ss-xl rounded-ee-xl" >{data.tag}</p>
-                            <Image src={data.imageurl} className="h-48 w-48 mx-auto mt-5 object-contain" alt={`Image of ${data.text}`} width={500} height={500} />
-                            <div className='mx-6'>
+                        <div
+                            onClick={() => route(data.id)}
+                            className='border border-1 border-[#ADADAD] rounded-xl sm:hover:shadow-xl hover:cursor-pointer'
+                            key={id}
+                        >
+                            <p className="bg-[#F74B81] text-white w-20 py-[0.11rem] text-center rounded-ss-xl rounded-ee-xl">
+                                {data.tag}
+                            </p>
+                            <Image
+                                src={data.imageurl}
+                                className="ssm:h-32 ssm:w-32 sm:h-48 sm:w-48 mx-auto sm:mt-5 object-contain"
+                                alt={`Image of ${data.text}`}
+                                width={500}
+                                height={500}
+                            />
+                            <div className='sssms:mx-2 sm:mx-3'>
                                 <p className='text-sm text-[#ADADAD]'>{data.category}</p>
-                                <h3 className='text-primaryText font-semibold hover:cursor-pointer h-20'>{data.text}</h3>
-                                <section >
+                                <h3 className='text-primaryText sm:font-semibold hover:cursor-pointer sm:h-12 ssm:text-sm sm:text-base sssms:h-10 ssm:h-10 ssms:h-10 line-clamp-2'>
+                                    {data.text}
+                                </h3>
+                                <section>
                                     <Rating value={data.rating} readOnly fractions={2} />
-                                    <p>By <span className='text-[#3BB77E] hover:cursor-pointer hover:underline'>{data.vendor}</span></p>
-                                    <section className='flex items-center mt-5 mb-8 text-md'>
-                                        <p className='text-[#3BB77E] font-semibold'>{`$  ${data.price}`}</p>
-                                        <p className='text-[#ADADAD] ml-2 font-semibold'><s>{`$ ${data.changedprice}`}</s></p>
-                                        <Link href={"/"} className='text-primary text-sm bg-[#DEF9EC] flex items-center py-2 px-5 ml-auto rounded-[0.2rem]'>
-                                            <Image src={"/images/popular/cart.svg"} width={500} height={500} className='w-5 h-5' alt='Cart Logo' />
+                                    <p>
+                                        By <span className='text-[#3BB77E] hover:cursor-pointer hover:underline'>{data.vendor}</span>
+                                    </p>
+                                    <section className='sm:flex items-center ssm:mb-4 ssm:mt-1 sm:mb-5 text-md'>
+                                        <section className='flex items-center ssm:space-x-5 xl:space-x-2'>
+                                            <p className='text-[#3BB77E] font-semibold'>{`$  ${data.price}`}</p>
+                                            <p className='text-[#ADADAD] sm:ml-2 font-semibold'>
+                                                <s>{`$ ${data.changedprice}`}</s>
+                                            </p>
+                                        </section>
+                                        <Link href={"/"} className='text-primary text-sm bg-[#DEF9EC] sm:flex items-center py-2 px-5 ml-auto rounded-[0.2rem] ssm:hidden'>
+                                            <Image
+                                                src={"/images/popular/cart.svg"}
+                                                width={500}
+                                                height={500}
+                                                className='w-5 h-5'
+                                                alt='Cart Logo'
+                                            />
                                             Add
+                                        </Link>
+                                        <Link href={"/"} className='text-primary text-sm bg-[#DEF9EC] ssm:flex items-center py-2 px-5 ml-auto rounded-[0.4rem] sm:hidden font-bold justify-center'>
+                                            Add to Cart
                                         </Link>
                                     </section>
                                 </section>
                             </div>
                         </div>
+
                     )
                 })}
             </div>
