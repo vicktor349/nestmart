90631cda0804e9eaae7f7b6e69494156367babac
vicktor349 <ayomidevictor349@gmail.com>
2024-07-23 00:10:54 +0100

    added notifications globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps })
 {
   return (
     <div>
       <MantineProvider>
+        <Notifications />
         <Layout>
           <Component {...pageProps} />
         </Layout>
