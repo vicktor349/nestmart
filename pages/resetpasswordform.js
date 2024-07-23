35a5a31a1a41b1d7b9832b7856a47c481cd6e69b
vicktor349 <ayomidevictor349@gmail.com>
2024-07-23 15:04:35 +0100

    Reset password page and also added logic

---
+++ pages/resetpasswordform.js
@@ -0,0 +1,99 @@
+import supabase from '@/config/supabase'
+import Head from 'next/head'
+import React, { useState } from 'react'
+import { showNotification } from '@mantine/notifications'
+import { useRouter } from 'next/router'
+import { Loader } from '@mantine/core'
+import { HiOutlineLockClosed } from 'react-icons/hi'
+
+const resetpasswordform = () =>
+{
+    const { push } = useRouter()
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const handleSubmit = async (e) =>
+    {
+        setIsSubmitting(true)
+        e.preventDefault()
+        if (newPassword !== confirmPassword)
+        {
+            showNotification({
+                title: 'Error',
+                message: 'Passwords do not match.',
+                color: 'red',
+            })
+            return
+        }
+
+        const { error } = await supabase.auth.updateUser({ password: newPassword })
+
+        if (error)
+        {
+            showNotification({
+                title: 'Error',
+                message: error.message,
+                color: 'red',
+            })
+        } else
+        {
+            showNotification({
+                title: 'Success',
+                message: 'Password updated successfully!',
+                color: 'green',
+            })
+            setIsSubmitting(!isSubmitting)
+            push('/signin')
+        }
+        setIsSubmitting(false)
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Nest | Reset Password</title>
+            </Head>
+            <div className='flex justify-center items-center'>
+                <div className='grid place-items-center border w-[30rem] px-10 py-28 space-y-5'>
+                    <div className='border border-black border-solid rounded-full w-32 h-32 flex items-center justify-center'><HiOutlineLockClosed size={72} /></div>
+                    <p>Create A Strong Password</p>
+                    <p className='text-gray-400'>Your password must be at least 6 characters</p>
+                    <form className='w-96 space-y-5' onSubmit={handleSubmit}>
+                        <div className="mt-2">
+                            <input
+                                required
+                                id="new-password"
+                                name="new-password"
+                                type="password"
+                                placeholder='Enter new password'
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 outline-none"
+                            />
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="confirm-password"
+                                name="confirm-password"
+                                type="password"
+                                placeholder='Confirm new password'
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 outline-none"
+                            />
+                        </div>
+                        <div>
+                            <button type="submit" disabled={isSubmitting} className="flex w-full justify-center items-center rounded-md bg-primary h-10 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                {isSubmitting ? <Loader size={16} color='white' /> : "Update Password"}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default resetpasswordform
