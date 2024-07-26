b307361d1927fd76319b8cb53c60572f2ed83247
vicktor349 <ayomidevictor349@gmail.com>
2024-07-26 09:51:06 +0100

    SideNavbar for user dashboard

---
+++ components/dashboard/SideNavbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BiMessageDetail } from 'react-icons/bi'
+import { CiMail } from 'react-icons/ci'
+import { FiUser } from 'react-icons/fi'
+import { GoHome } from 'react-icons/go'
+import { PiHeart, PiShoppingCartSimple } from 'react-icons/pi'
+import { RiMoneyDollarCircleFill } from 'react-icons/ri'
+
+const SideNavbar = ({ activeComponent, setActiveComponent }) =>
+{
+    const menuItems = [
+        { location: "Dashboard", icon: <GoHome size={24} />, name: "Dashboard" },
+        { location: "Profile", icon: <FiUser size={24} />, name: "Profile" },
+        { location: "Orders", icon: <PiShoppingCartSimple size={24} />, name: "Orders" },
+        { location: "Inbox", icon: <CiMail size={24} />, name: "Inbox" },
+        { location: "Reviews", icon: <BiMessageDetail size={24} />, name: "Reviews" },
+        { location: "SavedItems", icon: <PiHeart size={24} />, name: "Saved Items" },
+        { location: "Transactions", icon: <RiMoneyDollarCircleFill size={24} />, name: "Transactions" },
+    ]
+    return (
+        <div>
+            <ul className='mt-10 space-y-4'>
+                {menuItems.map((item) => (
+                    <li
+                        key={item.location}
+                        onClick={() => setActiveComponent(item.location)}
+                        className={`flex items-center select-none font-semibold space-x-4 hover:cursor-pointer w-64 py-5 text-[#666] hover:rounded-md text-sm ${activeComponent === item.location ? 'bg-primary/25 rounded-md duration-300 ease-in' : 'hover:bg-primary/25'}`}
+                    >
+                        <p className={`ml-5 ${activeComponent === item.location ? 'text-primary rounded-md duration-300 ease-in' : ''}`}>{item.icon}</p>
+                        <p className={`${activeComponent === item.location ? 'text-primary rounded-md duration-300 ease-in' : ''}`}>{item.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SideNavbar
