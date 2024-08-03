1548a00fa4ff0592379e130852c44bf444181c76
vicktor349 <ayomidevictor349@gmail.com>
2024-08-03 11:13:55 +0100

    Removed category navbar from cart page

--- components/Layout.js
+++ components/Layout.js
@@ -9,13 +9,13 @@ const Layout = ({ children }) =>
 {
     const router = useRouter();
     const Displayed = router.pathname !== '/signin' && router.pathname !== '/signup' && router.pathname !== '/404' && router.pathname !== '/resetpassword' && router.pathname !== '/resetpasswordform' && router.pathname !== '/dashboard';
-
+    const Display = router.pathname !== '/cart'
     return (
         <div>
             <ScrollToTop />
             <Navbar />
             <div className='ssm:mx-4 sm:mx-8 md:mx-16 lg:mx-20 xl:mx-24 2xl:mx-48 max-w-screen-2xl'>
-                {Displayed && <CategoryNavbar />}
+                {Display && Displayed && <CategoryNavbar />}
                 {children}
                 {Displayed && <Footer />}
             </div>
